5f9ccc43e8f15079110ad1cebcdaa59073b8662e
=divinepraises <=officebyzantin@gmail.com>
2026-03-18 23:38:27 -0700

    midnight pashal hour fix

--- midnight.js
+++ midnight.js
@@ -9,6 +9,6 @@ export function renderMidnightSkeleton() {
 
 export async function enhanceMidnight(priest, full, date){
 	let [year, mm, dd, season, seasonWeek, glas, dayOfWeek, dateAddress] = getDayInfo(date, true);
-	if (season === "EasterWeek" && dayOfWeek > 0) document.getElementById("beginning").innerHTML =  await EasterHour("compline", priest, full, date);
+	if (season === "EasterWeek" && dayOfWeek > 0) document.getElementById("beginning").innerHTML =  await EasterHour("midnight", priest, full, date);
 	else document.getElementById("beginning").innerHTML = "This hour is not ready yet."
 }
